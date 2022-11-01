7a02df4dd75ab027f4d02110a6e25ab1618ddfe1
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-11-01 17:13:15 +0100

    Aggiunto style e animazioni

--- js/script.js
+++ js/script.js
@@ -8,18 +8,25 @@ createApp({
                text: '',
                done: false,
             },
+            animationActive: false,
             tasks: [
                 {
-                    text: "Task 1",
+                    text: "Tank up",
                     done: true
                 }, {
-                    text: "Task 2",
+                    text: "Check fuel level",
                     done: false
                 }, {
-                    text: "Task 3",
+                    text: "Test the engines",
                     done: false
                 }, {
-                    text: "Task 4",
+                    text: "Bring enough space food",
+                    done: false
+                }, {
+                    text: "You sure you have enough fuel? Check again",
+                    done: false
+                }, {
+                    text: "Bring umbrellas to fight aliens",
                     done: false
                 }
             ]
@@ -35,13 +42,14 @@ createApp({
                 this.error = false;
             } else {
                 this.error = true;
-            }
-
-            let emptyClone = {...this.newTask} 
-            emptyClone.text = '';       
+            } 
+            this.newTask = '';
         },
         changeStatus(index) {
             this.tasks[index].done = !this.tasks[index].done;
+        },
+        rocketEntrance() {
+            this.animationActive = true;
         }
     }
 }).mount('#app')
